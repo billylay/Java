a5c144694cf5f1f83098c5ac22b073552dd36dbe
billylay <billy.lay0810@gmail.com>
2019-11-22 23:15:25 +0800

    0.1

---
+++ src/main/java/com/bill/learn/java/Simple.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.bill.learn.java;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author User
+ *
+ */
+public class Simple {
+
+	private static Logger log = Logger.getLogger(Simple.class);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.info("main");
+
+	}
+
+}
